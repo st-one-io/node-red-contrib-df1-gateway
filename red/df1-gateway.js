2b24a310a5fc23e07449bd7551b8245bab76c5c3
Fernando Amorim <fernandogabriel1703@gmail.com>
2022-04-11 15:12:36 -0300

    Create node-red-contrib-df1-gateway

---
+++ red/df1-gateway.js
@@ -0,0 +1,96 @@
+const Gateway = require('node-df1-gateway').gatewayConnect
+
+module.exports = function (RED) {
+    function df1Gateway(config) {
+        RED.nodes.createNode(this,config);
+        
+        const server = new Gateway({productName: config.nameGateway})
+        const endpoint = RED.nodes.getNode(config.endpoint);
+        let that = this;
+        let serverClosed = true;
+
+        endpoint.on('connected', () => {
+            endpoint.getDf1Session()
+            .then((res) => {
+                registerSession(res);
+            });
+        });
+
+        endpoint.on('error', () => {
+            endpoint.getDf1Session()
+            .then((res) => {
+                unRegisterSession(res);
+            });
+        });
+
+        this.on('close',() => {
+            if (serverClosed == false) {
+                closeServer();
+            };
+        });
+
+        server.on('error',(err) => {
+            if (serverClosed == false) {
+                closeServer();
+            };
+        });
+
+        server.on('connection',(conn) => {   
+            that.status(manageStatus('online',conn));
+        });
+
+        server.on('open', () => {
+            serverClosed = false;
+            that.status(manageStatus('online',0));
+        });
+
+        if(serverClosed == true) {
+            server.open();
+        };
+
+        function registerSession(df1) {
+            server.registerDf1(df1);
+        };
+
+        function unRegisterSession(df1) {
+            server.unRegisterDf1(df1);
+        };
+
+        function closeServer(){
+            serverClosed = true;
+
+            server.unRegisterAllDf1();
+
+            server.on('close', () => {
+                that.status(manageStatus('offline',0));
+            });
+            server.close();
+        };
+    };
+
+    function manageStatus(status,conn) {
+        let obj;
+
+        switch(status){
+            case 'online':{
+                obj = {
+                    fill: 'green',
+                    shape: 'dot',
+                    text: 'Conn: '+conn
+                };
+                break;
+            };
+            case 'offline':{
+                obj = {
+                    fill: 'red',
+                    shape: 'dot',
+                    text: 'Conn: '+conn
+                };
+                break;
+            };
+        };
+        return obj;
+    };
+
+    RED.nodes.registerType('df1 gateway', df1Gateway);
+}
